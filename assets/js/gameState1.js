1979c4fe84488eb296a248a2de4f3f3fda43248f
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2015-05-02 18:30:02 -0700

    added physics to state 1 and added some assets

--- assets/js/gameState1.js
+++ assets/js/gameState1.js
@@ -1,26 +1,55 @@
 var gameState1 = function(game) {};
 
+var map;
+var layer;
+
+var cursors;
+var sprite;
+
 gameState1.prototype = {
-    preload: function () {
-        //stuff to be loaded
-        this.load.image("pig", "assets/img/pig.png");
-        this.load.image("next", "assets/img/next.png");
-    },
+  preload: function () {
+    //stuff to be loaded
+    this.load.image("star", "assets/img/star.png");
+    this.load.image("next", "assets/img/next.png");
+  },
 
-    create: function () {
-        //sprites to be created
-        this.background = this.game.add.sprite(0, 0, "pig");
-        this.timer = 0;
-        this.button = this.game.add.button(400, 350, "next", this.nextState, this);
-        this.button.anchor.set(0.5, 0.5);
-    },
+  create: function () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //sprites to be created
+    sprite = game.add.sprite(0, 0, "star");
+    game.physics.enable(sprite);
 
-    nextState: function () {
-        this.game.state.start("GameState2");
-    },
+    cursors = game.input.keyboard.createCursorKeys();
 
-    update: function () {
-        //things for game to check
+    this.button = this.game.add.button(400, 350, "next", this.nextState, this);
+    this.button.anchor.set(0.5, 0.5);
+  },
+
+  nextState: function () {
+    this.game.state.start("GameState2");
+  },
+
+  update: function () {
+    //things for game to check
+    game.physics.arcade.collide(sprite, layer);
+
+    sprite.body.velocity.x = 0;
+    sprite.body.velocity.y = 0;
+    sprite.body.angularVelocity = 0;
+
+    if (cursors.left.isDown)
+    {
+        sprite.body.angularVelocity = -200;
     }
+    else if (cursors.right.isDown)
+    {
+        sprite.body.angularVelocity = 200;
+    }
+
+    if (cursors.up.isDown)
+    {
+        sprite.body.velocity.copyFrom(game.physics.arcade.velocityFromAngle(sprite.angle, 300));
+    }
+  }
 
 }
